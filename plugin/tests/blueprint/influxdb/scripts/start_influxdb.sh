9b4a975543ad834c997d8d01b7165d2f977572e8
fogelomer <omer@gigaspaces.com>
2016-04-04 10:32:42 +0300

    first commit

---
+++ plugin/tests/blueprint/influxdb/scripts/start_influxdb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+. $(ctx download-resource "components/utils")
+
+start_service influxdb
+
+sleep 10 #waiting for influxdb server to start
+
+if [ $(ctx node id) = 'influxdb_master' ]; then
+	ctx logger info "InfluxDB server is up, creating database name 'db1'..."
+	influx -host $(ctx instance host_ip) -execute="CREATE DATABASE telegraf"
+fi
