2533e60d47f1597ed59243004d1fa783a0386eae
fogelomer <omer@gigaspaces.com>
2016-05-04 16:48:55 +0300

    demo: dokcer, es inputs. gihk BP output

--- telegraf_plugin/tests/blueprint/influxdb/scripts/create_influxdb_centos.sh
+++ telegraf_plugin/tests/blueprint/influxdb/scripts/create_influxdb_centos.sh
@@ -3,13 +3,12 @@
 
 cat <<EOF | sudo tee /etc/yum.repos.d/influxdb.repo
 [influxdb]
-name = InfluxDB Repository - RHEL \$releasever
-baseurl = https://repos.influxdata.com/rhel/\$releasever/\$basearch/stable
+name = InfluxDB Repository - RHEL 7
+baseurl = https://repos.influxdata.com/rhel/7/x86_64/stable
 enabled = 1
 gpgcheck = 1
 gpgkey = https://repos.influxdata.com/influxdb.key
 EOF
-
 
 sudo yum install influxdb
 sudo service influxdb start
